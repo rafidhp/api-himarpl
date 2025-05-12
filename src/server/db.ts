1f14e88bab86adf93134564c0254706fdb77b04b
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2025-05-12 18:23:11 +0700

    fix: invalid db config

--- src/server/db.ts
+++ src/server/db.ts
@@ -7,17 +7,7 @@ const adapter = new PrismaLibSQL({
   authToken: `${env.TURSO_AUTH_TOKEN}`,
 });
 
-const createPrismaClient = () =>
-  new PrismaClient({
-    adapter,
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
-};
-
-export const db = globalForPrisma.prisma ?? createPrismaClient();
-
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+export const db = new PrismaClient({
+  adapter,
+  log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+});
