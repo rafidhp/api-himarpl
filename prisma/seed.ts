db8c7a62296523beb77d4466a99868d2a0e44997
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2025-05-12 18:46:54 +0700

    chore: add template seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,17 @@
 import { db } from "@/server/db";
 
-async function main() {}
+async function main() {
+  // Create a test user as author
+  const user = await db.user.findFirst({
+    where: {
+      email: "himarpl@upi.edu",
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+}
 
 main()
   .then(() => {
